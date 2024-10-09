d8898e3bd2a03e7ac381edd1edce3aa9cae15883
nidhihcl75 <kumari12nid02@gmail.com>
2024-10-09 15:26:37 +0530

    Update SoftVideoDecoderOMXComponent.cpp

--- media/libstagefright/omx/SoftVideoDecoderOMXComponent.cpp
+++ media/libstagefright/omx/SoftVideoDecoderOMXComponent.cpp
@@ -616,6 +616,13 @@ OMX_ERRORTYPE SoftVideoDecoderOMXComponent::getConfig(
                 DescribeHDR10PlusInfoParams* outParams =
                         (DescribeHDR10PlusInfoParams *)params;
 
+                if (offsetof(DescribeHDR10PlusInfoParams, nValue) + outParams->nParamSize >
+                    outParams->nSize) {
+                    ALOGE("b/329641908: too large param size; nParamSize=%u nSize=%u",
+                          outParams->nParamSize, outParams->nSize);
+                    android_errorWriteLog(0x534e4554, "329641908");
+                    return OMX_ErrorBadParameter;
+                }
                 outParams->nParamSizeUsed = info->size();
 
                 // If the buffer provided by the client does not have enough
